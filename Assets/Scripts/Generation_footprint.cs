bbff69f921c142470cadee2fd24d3e09ea789f6d
EleouetClement <77010567+EleouetClement@users.noreply.github.com>
2021-10-28 12:05:21 +0200

    Initialisation L-System

---
+++ Assets/Scripts/Generation_footprint.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation_footprint : MonoBehaviour
+{
+    // Building's footprint generation using L-system
+    [SerializeField][Range(0, 10)] private int width;
+    [SerializeField][Range(0, 10)] private int depth;
+    [SerializeField][Range(0, 10)] private int height;
+    [SerializeField] [Range(0, 2)] private int step;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MoveForward()
+    {
+        //Move the current cursor then put a new point at the location
+
+    }
+
+    private void TurnLeft()
+    {
+        //Apply a negative rotation to the cursor
+    }
+
+    private void TurnRight()
+    {
+        //Apply a positive rotation to the cursor
+    }
+
+}
